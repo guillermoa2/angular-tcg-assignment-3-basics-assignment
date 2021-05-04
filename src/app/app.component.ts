82e818675e740d6e1555dcd56c190257ba98d77b
Guillermo Acosta <guillermo222@att.net>
2021-05-04 13:31:02 -0700

    assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    .whiteLogs {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
+  displayDetails: boolean = true;
+  count: number = 1
+  logs: number[] = []
+
+  paraToggle () {
+    this.displayDetails = !this.displayDetails;
+    this.logs.push(this.count++)
+  }
+
+  getColor(log) {
+    return (log >= 5 ? "blue" : "white")
+  }
 }
